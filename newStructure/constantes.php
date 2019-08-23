a5a05a40dee279e2f6faa28b990ee59a31eea608
Tecnosoft Computers <tecnosoftComputersDesarrollo@gmail.com>
2019-08-23 13:30:19 -0500

    Merge 1 FF y MV 2

--- newStructure/constantes.php
+++ newStructure/constantes.php
@@ -1,6 +1,5 @@
 <?php
 define('PUERTO', 'http');
-<<<<<<< HEAD
 define('HOST', 'localhost/fernando/acf/newStructure'); 
 define('FRONTEND_RUTA', 'c:/xampp/htdocs/fernando/acf/newStructure/');
 //define('DBSERVIDOR', '186.3.133.147');
@@ -10,28 +9,11 @@ define('DBSERVIDOR', 'localhost');
 define('DBUSUARIO', 'root'); 
 define('DBCLAVE', '');  
 define('DBNOMBRE', 'tecnoso1_acf');   
-=======
-define('HOST', 'localhost/fernanda/acf/newStructure'); 
-define('FRONTEND_RUTA', 'C:/xampp/htdocs/fernanda/acf/newStructure/');
-//define('DBSERVIDOR', 'imbabura2');
-//define('DBUSUARIO', 'tecnoso1_tecnosoft'); 
-//define('DBCLAVE', '$cambio2015$x');  
-define('DBSERVIDOR', 'localhost');
-define('DBUSUARIO', 'root'); 
-define('DBCLAVE', '');  
-define('DBNOMBRE', 'acf_localff2');   
->>>>>>> FF
 define('RUTA_INCLUDES', FRONTEND_RUTA.'includes/');
 define('RUTA_FRONTEND', FRONTEND_RUTA.'frontend/'); 
 define('RUTA_VISTA', FRONTEND_RUTA.'frontend/Vista/');
 define('KEY_ENCRIPTAR','acfecuador123456');
-<<<<<<< HEAD
 define('PREVIOUS_SYSTEM','localhost/fernando/acf/');
 define('DASHBOARD', 'inicializador/vistas/app/in.php');
-=======
-define('PREVIOUS_SYSTEM','localhost/fernanda/acf/');
-/*define('STATE', array('1'=>'Active','0'=>'Inactive'));
-define('TYPE_TABLE', array('1'=>'Active','0'=>'Inactive'));
-define('TYPE_RENT', array('A'=>'Cap.+Int.','B'=>'Int.'));*/
->>>>>>> FF
+
 ?>
